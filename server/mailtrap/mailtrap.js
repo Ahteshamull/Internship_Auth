ac12e36d38b2e3f0edb334ed1f11b0ccd570dce5
Hasan <158465711+Ahteshamull@users.noreply.github.com>
2025-04-18 03:09:55 +0600

    email send successfully

---
+++ server/mailtrap/mailtrap.js
@@ -0,0 +1,30 @@
+import { MailtrapClient } from "mailtrap";
+import dotenv from "dotenv";
+dotenv.config();
+
+
+const TOKEN = "6c3108428ccfd90492e34e08fc8c1a73";
+
+const client = new MailtrapClient({
+  token: TOKEN,
+});
+
+const sender = {
+  email: "hello@demomailtrap.co",
+  name: "Mailtrap Test",
+};
+const recipients = [
+  {
+    email: "ahteshamulhasan2@gmail.com",
+  },
+];
+
+client
+  .send({
+    from: sender,
+    to: recipients,
+    subject: "You are awesome!",
+    text: "Congrats for sending test email with Mailtrap!",
+    category: "Integration Test",
+  })
+  .then(console.log, console.error);
