bd7c807e80af6611c4f6cea5c8a5b53dc5afcf75
Hasan <158465711+Ahteshamull@users.noreply.github.com>
2025-04-18 00:37:46 +0600

    Model create done

---
+++ server/models/userModel.js
@@ -0,0 +1,44 @@
+import { Schema } from "mongoose";
+const userSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true, 
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    lastLogin: {
+
+        type: Date,
+        default: Date.now
+    },
+    isVerify: {
+        type: Boolean,
+        default: false
+    },
+    resetPasswordToken: {
+        type: String,
+        
+    },
+    resetPasswordExpiresAt: {
+        type: Date,
+        default: Date.now
+    },
+    verificationToken: {
+        type: String,
+      
+    },
+    verificationTokenExpiresAt: {
+        type: Date,
+        default: Date.now
+    }
+    
+}, { timestamps: true });
+
+export const User = mongoose.model("AuthUser", userSchema); 
