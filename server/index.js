860c952b018e20a6d1a5200a050353bed89d6261
Hasan <158465711+Ahteshamull@users.noreply.github.com>
2025-04-21 03:01:10 +0600

    Project Complete

--- server/index.js
+++ server/index.js
@@ -3,12 +3,13 @@ import { dbConnectDb } from "./db/dbConnect.js";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+// import path from "path";
 
 
 import authRoutes from "./routes/auth.routes.js"
 dotenv.config();
 const app = express();
-
+// const __dirname = path.resolve();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json())
@@ -21,6 +22,14 @@ app.use(cors(
 ))
 
 app.use("/api/auth", authRoutes)
+
+
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "/client/dist")));
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
+//   });
+// }
 
 
 app.listen(PORT, () => {
